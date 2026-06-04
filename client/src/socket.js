6da1832c3c994d675070106ca3635628e1feeeac
Bruno <bruno@email.com>
2026-06-03 22:56:39 -0300

    Fix: Use environment variable for Socket.IO connection URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,10 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3001');
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+
+export const socket = io(API_URL, {
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  reconnectionAttempts: 5
+});
